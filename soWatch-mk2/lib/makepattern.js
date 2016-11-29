0d1f9e668af78a826dd23d57e3d536b8573136e5
jc3213 <jc3213@users.noreply.github.com>
2016-11-29 18:04:08 +0800

    Update makepattern.js

--- soWatch-mk2/lib/makepattern.js
+++ soWatch-mk2/lib/makepattern.js
@@ -2,27 +2,13 @@
 
 function makeRegExp (string) {
   var pattern = string.replace(/\//g, "\\/").replace(/\?/g, "\\?").replace(/\./g, "\\.").replace(/\*/g, ".*");
-  return new RegExp(pattern, "i");
-}
-
-function worker (data) {
-  if (typeof data == "string") {
-    return makeRegExp(rule);
-  } else {
-    return data;
-  }	
+  return new RegExp(pattern);
 }
 
 exports.encode = function (data) {
-  if (typeof data == "array") {
-    var array = new Array();
-    data.forEach(function (element, index, array) {
-      var rule = element[0];
-      var pattern = worker(rule);
-      array.push(pattern);
-    }
-    return array;
-  ) else (
-    return worker(data);
-  )
+  if (typeof data == "string") {
+    return makeRegExp(data);
+  } else {
+    return data;  
+  }
 };
